64e3d84ca8405a5d274f95dd17fb84282710a426
Julia <72133073+yuliamarkiv@users.noreply.github.com>
2020-12-15 22:06:10 +0200

    Create 0181.cpp

---
+++ 0181.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int  M , N  ;
+  cin >> N >> M ;
+  if (  ( N * M ) % 2 == 0   ) 
+    {
+        cout << " Dragon " <<endl;
+    }
+    else 
+    {
+        cout<< "Imp "<<endl;
+    }
+
+  
+ return 0;
+}
