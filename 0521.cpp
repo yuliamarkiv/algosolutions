7165bd4fdc9b67733910804832a809717e239326
Julia <72133073+yuliamarkiv@users.noreply.github.com>
2020-12-15 22:08:36 +0200

    Create 0521.cpp

---
+++ 0521.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+  int  A , B , C , D ,  sum ;
+  cin >> A >> B >> C >> D ;
+  sum = A + B + C + D ;
+  cout << sum << endl;
+  
+ return 0;
+}
